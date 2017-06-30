4461f36e98802f0c5ad9101359f6122757f6eca4
José Luis Massón <jlmasson@espol.edu.ec>
2017-06-30 01:21:14 -0500

    Primos generados

---
+++ ejercicio.py
@@ -0,0 +1,21 @@
+numeroImpar = int(input("Número impar: "))
+
+maximoPermitido = numeroImpar // 2
+
+listaPrimos = []
+
+for i in range(2, maximoPermitido + 1):
+
+	c = 0
+
+	for j in range(1, i):
+
+		if i % j == 0:
+
+			c += 1
+
+	if c == 1:
+
+		listaPrimos.append(i)
+
+print(listaPrimos)
